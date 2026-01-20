c88b74997b126ad036ee33db078513d693fc0921
LinaRaihanah <18DIT24F1027@student.ptss.edu.my>
2026-01-20 10:03:30 +0800

    update data edit user information fix content error

---
+++ biodata.php
@@ -0,0 +1,17 @@
+<?php
+$nama = "AMALINA RAIHANAH BINTI ROHIMI";
+$nomatrik = "18DIT24F1027";
+$kelas = "DIT4A";
+?>
+
+<div class="container">
+    <h2>Maklumat Pelajar</h2>
+
+    <img src="gambar.jpg" alt="Gambar Pelajar">
+
+    <p><strong>Nama:</strong> <?= $nama ?></p>
+    <p><strong>No Matrik:</strong> <?= $nomatrik ?></p>
+    <p><strong>Kelas:</strong> <?= $kelas ?></p>
+
+    <p><strong>Program:</strong> DIPLOMA TEKNOLOGI MAKLUMAT</p>
+</div>
